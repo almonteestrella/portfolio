64427d9185e76a2cfa052cc92e14c0aba644ea9d
Juan Almonte <almonteestrella@gmail.com>
2023-04-26 08:39:50 -0400

    skills section added

--- src/components/TechLogo.jsx
+++ src/components/TechLogo.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 import styled from 'styled-components';
 
-const TechLogo = ({ image }) => {
+const TechLogo = ({ image, name }) => {
     return (
         <Container>
             <div className='logo'>
@@ -14,6 +14,7 @@ const TechLogo = ({ image }) => {
                     className='logo-img'
                 />
             </div>
+            <h4 className='name'>{name}</h4>
         </Container>
     );
 };
@@ -21,12 +22,32 @@ const TechLogo = ({ image }) => {
 const Container = styled.div`
     .logo {
         background: var(--body-bg-color);
+        padding: 0.5rem;
+        border-radius: 10px;
+        margin-bottom: 1rem;
+        display: grid;
+        place-items: center;
+        cursor: pointer;
+        transition: var(--transition);
+    }
+
+    .logo:hover {
+        transform: scale3d(1.1, 0.8, 1.1);
     }
 
     .logo-img {
-        width: 100px;
-        height: 100px;
+        width: 100%;
+        border-radius: 50%;
+        height: 100%;
+
         object-fit: cover;
+    }
+
+    .name {
+        color: var(--text-color);
+        opacity: 0.8;
+        text-transform: capitalize;
+        text-align: center;
     }
 `;
 export default TechLogo;
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,3 +1,4 @@
+import TechLogo from '@/components/TechLogo';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -5,7 +6,28 @@ const Skills = () => {
     return (
         <SkillContainer>
             <main>
-                <div className='content'></div>
+                <div className='title'>
+                    <h2>technologies I implement</h2>
+                </div>
+                <div className='grid-content'>
+                    <TechLogo image={'/express.svg'} name={'express'} />
+                    <TechLogo image={'/js.svg'} name={'javascript'} />
+                    <TechLogo image={'/materialui.svg'} name={'materia ui'} />
+                    <TechLogo image={'/mern.svg'} name={'mern'} />
+                    <TechLogo image={'/mongo.svg'} name={'mongoDB'} />
+                    <TechLogo image={'/motion.svg'} name={'framer motion'} />
+                    <TechLogo image={'/next.svg'} name={'next'} />
+                    <TechLogo image={'/node.svg'} name={'node'} />
+                    <TechLogo image={'/react.svg'} name={'react'} />
+                    <TechLogo image={'/RTK.svg'} name={'RTK'} />
+                    <TechLogo image={'/html&css.svg'} name={'html & css'} />
+                    <TechLogo image={'/figma.svg'} name={'figma'} />
+                    <TechLogo
+                        image={'/styledcomponent.svg'}
+                        name={'styled components'}
+                    />
+                    <TechLogo image={'/tailwind.svg'} name={'tailwind'} />
+                </div>
             </main>
         </SkillContainer>
     );
@@ -17,6 +39,27 @@ const SkillContainer = styled.div`
     main {
         width: 80vw;
         margin: 0 auto;
+    }
+
+    .title {
+        position: relative;
+        margin-bottom: 3rem;
+        &::after {
+            content: '';
+            display: block;
+            position: absolute;
+            top: 20px;
+            width: 300px;
+            height: 2px;
+            left: 350px;
+            background-color: var(--primary-color);
+        }
+    }
+
+    .grid-content {
+        display: grid;
+        gap: 2rem;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
     }
 `;
 
